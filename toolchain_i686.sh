1989077641a9c97606264dcb5dabbb57f3d9e666
The Jat <manishkl543@gmail.com>
2025-09-14 23:52:41 +0530

    Improve toolchain setup: faster downloads and parallel build support

--- toolchain_i686.sh
+++ toolchain_i686.sh
@@ -3,8 +3,6 @@
 # Set the installation prefix and target
 BASE_DIR=$(pwd)
 TOOLCHAIN_DIR="$BASE_DIR/i686"
-SRC_DIR="$TOOLCHAIN_DIR/src"
-BUILD_DIR="$TOOLCHAIN_DIR/build"
 PREFIX="$TOOLCHAIN_DIR"
 TARGET=i686-elf
 PATH="$PREFIX/bin:$PATH"
@@ -16,43 +14,48 @@ BINUTILS_VERSION=2.42
 # Directories for sources and build
 SRC_DIR="$HOME/src"
 BUILD_DIR="$SRC_DIR/build"
-mkdir -p $PREFIX $SRC_DIR $BUILD_DIR
+mkdir -p "$PREFIX" "$SRC_DIR" "$BUILD_DIR"
 
 # Function to download and extract a tarball
 download_and_extract() {
     local url=$1
-    local tarball=$(basename $url)
+    local tarball=$(basename "$url")
     local dirname=${tarball%.tar.*}
 
-    if [ ! -f $SRC_DIR/$tarball ]; then
-        wget -P $SRC_DIR $url
+    if [ ! -f "$SRC_DIR/$tarball" ]; then
+        wget -c -O "$SRC_DIR/$tarball" "$url"
     fi
-    if [ ! -d $SRC_DIR/$dirname ]; then
-        tar -xf $SRC_DIR/$tarball -C $SRC_DIR
+    if [ ! -d "$SRC_DIR/$dirname" ]; then
+        tar -xf "$SRC_DIR/$tarball" -C "$SRC_DIR"
     fi
 }
 
+# Fast mirrors
+GCC_URL="https://mirrors.kernel.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
+BINUTILS_URL="https://mirrors.kernel.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz"
+
 # Download GCC and Binutils sources
-download_and_extract https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz
-download_and_extract https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz
+download_and_extract "$BINUTILS_URL"
+download_and_extract "$GCC_URL"
 
 # Build and install Binutils
-mkdir -p $BUILD_DIR/binutils
-cd $BUILD_DIR/binutils
-$SRC_DIR/binutils-$BINUTILS_VERSION/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
-make
+mkdir -p "$BUILD_DIR/binutils"
+cd "$BUILD_DIR/binutils"
+"$SRC_DIR/binutils-$BINUTILS_VERSION/configure" \
+    --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
+make -j$(nproc)
 make install
 
-# Build and install GCC (Bootstrap)
-mkdir -p $BUILD_DIR/gcc
-cd $BUILD_DIR/gcc
-$SRC_DIR/gcc-$GCC_VERSION/configure --target=$TARGET --prefix=$PREFIX --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc
+# Build and install GCC (bootstrap)
+mkdir -p "$BUILD_DIR/gcc"
+cd "$BUILD_DIR/gcc"
+"$SRC_DIR/gcc-$GCC_VERSION/configure" \
+    --target=$TARGET --prefix=$PREFIX --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc -j$(nproc)
 make install-gcc
-make all-target-libgcc
+make all-target-libgcc -j$(nproc)
 make install-target-libgcc
 
 echo "Cross-compiler for $TARGET installed in $PREFIX"
+"$PREFIX/bin/$TARGET-gcc" --version
 
-# Verify the installation
-$PREFIX/bin/$TARGET-gcc --version
